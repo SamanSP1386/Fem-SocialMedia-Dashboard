97fdff88d2f28aab0a6ca130bc28023142d21dc9
saman <saman.sepehr86@gmail.com>
2026-01-05 21:50:23 -0800

    The first version of my work

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+function scssTask(){
+    return src('app/scss/**/*.scss', { sourcemaps : true})
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(),cssnano()]))
+        .pipe(dest('dist', { sourcemaps: '.'}))
+}
+
+function jsTask(){
+    return src('app/js/**/*.js', { sourcemaps : true})
+        .pipe(babel({ presets: ['@babel/preset-env']}))
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.'}))
+}
+
+function browserSyncServer(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+
+function browserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+function watchTask(){
+    watch('*.html', browserSyncReload);
+    watch(
+        ['app/scss/**/*.scss', 'app/**/*.js'],
+        series(scssTask, jsTask, browserSyncReload)
+    );
+}
+
+exports.default = series(scssTask, jsTask, browserSyncServer, watchTask);
---
+++ notes.md
@@ -0,0 +1,14 @@
+# functional Requirments
+
+Light/Dark mode toggle
+-What Html markup 
+-Switching between light/dark mode via Js
+-three option toggle : light/dark/system preference
+
+
+Css Variables (Csutom properties)
+
+Prefers-color-scheme media query
+Accessibility 
+-Use correct heading tags
+-Screenreader-only text for card titles/usernames
